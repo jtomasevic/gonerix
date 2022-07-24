459f334b1097d76ee548953d349a9bcaebfbc979
Jovan <jtomasevic@gmail.com>
2022-07-24 18:20:24 +0200

    Change stack to be based on slice

--- collections/priority_queue_test.go
+++ collections/priority_queue_test.go
@@ -16,58 +16,48 @@ func TestPriorityQueue_Enqueue(t *testing.T) {
 			3, 3, 4, 5, 7, 10, 10,
 		}, queue.priorities.ToList())
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "Tesla",
-					OrderNo:  "TSL-s2",
-					Priority: 3,
-				},
-				{
-					Customer: "Google",
-					OrderNo:  "G-s2",
-					Priority: 3,
-				},
+			{
+				Customer: "Tesla",
+				OrderNo:  "TSL-s2",
+				Priority: 3,
+			},
+			{
+				Customer: "Google",
+				OrderNo:  "G-s2",
+				Priority: 3,
 			},
 		}, queue.values[3])
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "IBM",
-					OrderNo:  "IBM-s1",
-					Priority: 4,
-				},
+			{
+				Customer: "IBM",
+				OrderNo:  "IBM-s1",
+				Priority: 4,
 			},
 		}, queue.values[4])
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "Google",
-					OrderNo:  "G-s1",
-					Priority: 5,
-				},
+			{
+				Customer: "Google",
+				OrderNo:  "G-s1",
+				Priority: 5,
 			},
 		}, queue.values[5])
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "Microsoft",
-					OrderNo:  "TSL-s2",
-					Priority: 7,
-				},
+			{
+				Customer: "Microsoft",
+				OrderNo:  "TSL-s2",
+				Priority: 7,
 			},
 		}, queue.values[7])
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "Apple",
-					OrderNo:  "Apple-s1",
-					Priority: 10,
-				},
-				{
-					Customer: "Tesla",
-					OrderNo:  "TSL-s1",
-					Priority: 10,
-				},
+			{
+				Customer: "Apple",
+				OrderNo:  "Apple-s1",
+				Priority: 10,
+			},
+			{
+				Customer: "Tesla",
+				OrderNo:  "TSL-s1",
+				Priority: 10,
 			},
 		}, queue.values[10])
 	})
@@ -94,12 +84,10 @@ func TestPriorityQueue_Enqueue(t *testing.T) {
 			3, 4, 5, 7, 10, 10,
 		}, queue.priorities.ToList())
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "Google",
-					OrderNo:  "G-s2",
-					Priority: 3,
-				},
+			{
+				Customer: "Google",
+				OrderNo:  "G-s2",
+				Priority: 3,
 			},
 		}, queue.values[3])
 		//
@@ -108,12 +96,10 @@ func TestPriorityQueue_Enqueue(t *testing.T) {
 			4, 5, 7, 10, 10,
 		}, queue.priorities.ToList())
 		require.Equal(t, Stack[Order]{
-			elements: []Order{
-				{
-					Customer: "IBM",
-					OrderNo:  "IBM-s1",
-					Priority: 4,
-				},
+			{
+				Customer: "IBM",
+				OrderNo:  "IBM-s1",
+				Priority: 4,
 			},
 		}, queue.values[4])
 		require.Equal(t, Order{
--- collections/stack.go
+++ collections/stack.go
@@ -1,13 +1,11 @@
 package collections
 
 // Stack Represents a simple last-in-first-out (LIFO) collection of objects.
-type Stack[T any] struct {
-	elements []T
-}
+type Stack[T any] []T
 
 // Push Inserts an object at the top of the Stack.
 func (stack *Stack[T]) Push(element T) {
-	stack.elements = append([]T{element}, stack.elements...)
+	*stack = append([]T{element}, *stack...)
 }
 
 // Pop Removes and returns the object at the top of the Stack.
@@ -16,8 +14,8 @@ func (stack *Stack[T]) Pop() *T {
 	if stack.IsEmpty() {
 		return nil
 	}
-	forPop := stack.elements[0]
-	stack.elements = stack.elements[1:len(stack.elements)]
+	forPop := (*stack)[0]
+	*stack = (*stack)[1:len(*stack)]
 	return &forPop
 }
 
@@ -27,10 +25,10 @@ func (stack *Stack[T]) Peek() *T {
 	if stack.IsEmpty() {
 		return nil
 	}
-	return &stack.elements[0]
+	return &(*stack)[0]
 }
 
 // IsEmpty returns true if stack is empty.
 func (stack *Stack[T]) IsEmpty() bool {
-	return len(stack.elements) == 0
+	return len(*stack) == 0
 }
